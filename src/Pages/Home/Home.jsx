ccd25037cfd96259fc48efbf715d5cee10f3bfff
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-05-11 23:57:34 +0600

    new section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/BannerSlider/Banner";
+import CTA from "../../components/CTA/CTA";
 
 
 const Home = () => {
     return (
         <div className='mx-1'>
             <Banner/>
+            <CTA/>
         </div>
     );
 };
